ee63cbb66e81b2dec802e17d6c1eaa2519248c91
LYSANDER-PC\Lysander <finblack20@gmai.com>
2020-10-15 17:00:57 -0400

    added client

--- index.js
+++ index.js
@@ -1,70 +1,22 @@
-
-const axios = require('axios');
-const cheerio = require('cheerio');
-const page_url = 'https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States';
+const express = require('express');
 
 
+const getUSStates = require('./getUSSates');
+const app = express();
 
 
-async function getUSSates(){
+//any static files get serverd
+app.use(express.static('client'));
 
-    const {data} = await axios.get(page_url);
+app.get('/api/state', async (req, res) => {
+    const states = await getUSStates();
 
-    
-
-    //data is html and past into cheerio $
-    const $ = cheerio.load(data);
-   const table =  $('caption:contains("States of the United States of America")').parent();
-
-   const states = [];
-    table.find('tbody tr').slice(2).each((i, element) => {
-       const $row = $(element);
-       const state = {};
-
-       state.name = $row.find('th a').first().text().trim();
-      
-       const labels = [
-        'code', 
-        'capital', 
-        'largest',
-        'reatification', 
-        'population', 
-        'total_area_miles',
-        'total_area_km',
-        'land_area_miles',
-        'land_area_km' ,
-        'water_area_miles',
-        'water_area_km',
-        'number_of_reps'];
+    res.json(states);
+});
 
 
-        let offset = 0;
-       $row.find('td').each((j, col) => {
-           const $col = $(col);
-          let value =  $col.text().trim();
-          //replace any ,
-          const numValue = Number(value.replace(/,/g, ''));
+const port = process.env.PORT || 4242;  
 
-          if(!isNaN(numValue)){
-              value = numValue;
-          }
-           if(j === 1 && $col.attr('colspan') === '2'){
-            const label = labels[j];
-            state[label] = value;
-               offset = 1; 
-           }
-           const label = labels[j + offset];
-           state[label] = value;
-          
-       });
-
-     
-       states.push(state);
-
-       console.log(state);
-   });
-
-   
-}
-
-getUSSates();
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}!`);
+});
